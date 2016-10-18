9115a164073411e8708505007234a29922d5373d
Pau <paserrod@gmail.com>
2016-10-19 00:01:39 +0200

    Prueba subir solucion de ejercicio 1

--- Ejercicio1/src/Buffer.java
+++ Ejercicio1/src/Buffer.java
@@ -10,19 +10,36 @@ class Buffer {
  
    public synchronized void put(char c) {
      while(count == buffer.length) ;
+     try {
+ 		wait();
+ 	} catch (InterruptedException e) {
+ 		// TODO Auto-generated catch block
+ 		e.printStackTrace();
+ 	}
      System.out.println("Produciendo " + c + " ...");
      buffer[in] = c; 
      in = (in + 1) % buffer.length; 
      count++;
+     notifyAll();
    }
     
    public synchronized char get() {
      while (count == 0) ;
+     try {
+ 		wait();
+ 	} catch (InterruptedException e) {
+ 		// TODO Auto-generated catch block
+ 		e.printStackTrace();
+ 	}
      char c = buffer[out]; 
      out = (out + 1) % buffer.length;
      count--;
+     
      System.out.println("Consumiendo "+ c + " ...");
+     notifyAll();
+     
      return c;
+     //solucionado
    }
 }
      
