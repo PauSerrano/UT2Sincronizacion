f0613a9c7083e82068ad7ae88dae1693473b5342
Pau <paserrod@gmail.com>
2016-10-19 00:00:11 +0200

    Prueba subir solucion

--- Ejercicio3/src/CoffeeStore.java
+++ Ejercicio3/src/CoffeeStore.java
@@ -6,14 +6,14 @@ public class CoffeeStore {
 	    private void someLongRunningProcess() throws InterruptedException {
 	        Thread.sleep(3000);
 	    }
-	     
-	    public void buyCoffee(String client) throws InterruptedException {
+	    //solucionado
+	    public synchronized void buyCoffee(String client) throws InterruptedException {
 	        someLongRunningProcess();
 	        lastClient = client;
 	        soldCoffees++;
 	        System.out.println(client + " bought some coffee");
 	    }
-	     
+	    //prueba commit
 	    public int countSoldCoffees() {return soldCoffees;}
 	     
 	    public String getLastClient() {return lastClient;}
